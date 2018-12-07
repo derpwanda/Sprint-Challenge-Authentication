dfa25df9407e1009a72dae0a200cf9db50bfe099
Wanda <doubleuevans@gmail.com>
2018-12-07 10:50:52 -0600

    added register and login routes; functional

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,10 @@
 const axios = require('axios');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
-const { authenticate } = require('./middlewares');
+const db = require('../database/dbConfig');
+
+const { authenticate, generateToken } = require('./middlewares');
 
 module.exports = server => {
   server.post('/api/register', register);
@@ -10,10 +14,35 @@ module.exports = server => {
 
 function register(req, res) {
   // implement user registration
+  const credentials = req.body;
+  const hash = bcrypt.hashSync(credentials.password, 4)
+
+  credentials.password = hash;
+
+  db('users')
+    .insert(credentials)
+    .then(user => {
+      res.status(201).json(user);
+    })
+    .catch(err => res.json({ message: 'registration failed', err }))
 }
 
 function login(req, res) {
   // implement user login
+  const credentials = req.body;
+  
+  db('users')
+    .where({ username: credentials.username })
+    .first()
+    .then(user => {
+      if(user && bcrypt.compareSync(credentials.password, user.password)) {
+        const token = generateToken(user);
+        res.status(200).json({ message: "login success", token })
+      } else {
+        res.status(401).json({ message: "incorrect inputs"})
+      }
+    })
+    .catch(() => res.json({ message: "login fail"}))
 }
 
 function getJokes(req, res) {
